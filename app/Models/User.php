5e60147ea8c96b7c57b17030d68df887547f4bee
Ali Arefzadeh <poryaarmanfar@gmail.com>
2024-08-09 12:54:06 +0330

    database finished

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -31,6 +32,24 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function videos():HasMany
+    {
+        return $this->hasMany(Video::class);
+    }
+
+    public function posts():HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments():HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+
+
 
     /**
      * Get the attributes that should be cast.
